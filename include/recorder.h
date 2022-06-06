408d9124fbe583df6655611d18aed26aaf2538b9
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-05 21:22:40 -0300

    starts to implement the recorder class

---
+++ include/recorder.h
@@ -0,0 +1,5 @@
+#include <QObject>
+
+class Recorder : public QObject {
+  Q_OBJECT
+};
--- src/recorder.cpp
+++ src/recorder.cpp
@@ -1,27 +1,27 @@
-#include <QGuiApplication>
-#include <QPixmap>
-#include <QRgb>
-#include <QScreen>
-#include <QString>
+// #include <QGuiApplication>
+// #include <QPixmap>
+// #include <QRgb>
+// #include <QScreen>
+// #include <QString>
 
-void one_shot() {
-  auto screens = QGuiApplication::screens();
-  auto index = 1;
-  for (auto screen : screens) {
-    QPixmap pixmap;
-    pixmap = screen->grabWindow(0);
-    auto image = pixmap.toImage();
-    auto size = image.width() * image.height();
-    auto pixels = (QRgb *)image.constBits();
-    for (auto i = 0; i < size; i++) {
-      auto pixel = pixels[i];
-      auto r = qRed(pixel);
-      auto g = qGreen(pixel);
-      auto b = qBlue(pixel);
-      qDebug() << "R:" << r << "G:" << g << "B:" << b << "\n";
-    }
-    auto filename = QString("screen%1.png").arg(index);
-    pixmap.save(filename);
-    index++;
-  }
-}
+// void one_shot() {
+//   auto screens = QGuiApplication::screens();
+//   auto index = 1;
+//   for (auto screen : screens) {
+//     QPixmap pixmap;
+//     pixmap = screen->grabWindow(0);
+//     auto image = pixmap.toImage();
+//     auto size = image.width() * image.height();
+//     auto pixels = (QRgb *)image.constBits();
+//     for (auto i = 0; i < size; i++) {
+//       auto pixel = pixels[i];
+//       auto r = qRed(pixel);
+//       auto g = qGreen(pixel);
+//       auto b = qBlue(pixel);
+//       qDebug() << "R:" << r << "G:" << g << "B:" << b << "\n";
+//     }
+//     auto filename = QString("screen%1.png").arg(index);
+//     pixmap.save(filename);
+//     index++;
+//   }
+// }
